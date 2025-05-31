f0a04d7abed70d127f163d2ec307de6ea5632603
AhmadKamaludin24 <ahmadkamaludin2428@gmail.com>
2025-05-31 14:50:07 +0700

    modified:   components/ui/Navbar.tsx

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -19,42 +19,48 @@ const Navbar = () => {
       </div>
 
       <div className="lg:hidden">
-        <button onClick={() => setIsOpen(!isOpen)} className="flex items-center justify-center p-2">
-          <AlignJustify/>
+        <button
+          onClick={() => setIsOpen(!isOpen)}
+          className="flex items-center justify-center p-2">
+          <AlignJustify />
         </button>
       </div>
 
-      { isOpen && (
-         <div className="lg:hidden absolute top-0 right-0 w-1/2 py-12 h-screen bg-white flex flex-col justify-between">
-        <div className="flex flex-col justify-start items-end gap-4 px-4" onClick={() => setIsOpen(false)}>
-          <button onClick={() => setIsOpen(false)} className="flex items-center justify-center p-2">
-            <X/>
-          </button>
-          <Link href={"/"}>Home</Link>
-          <Link href={"/companions"}>Companions</Link>
-          <Link href={"/my-journey"}>My journey</Link>
-         
-        </div>
-        <div className="flex w-full justify-end items-center px-4" onClick={() => setIsOpen(false)}>
-           {/* <Link href="/sign-in" className="w-full">
+      {isOpen && (
+        <div className="lg:hidden absolute top-0 right-0 w-1/2 py-12 h-screen bg-white flex flex-col justify-between">
+          <div
+            className="flex flex-col justify-start items-end gap-4 px-4"
+            onClick={() => setIsOpen(false)}>
+            <button
+              onClick={() => setIsOpen(false)}
+              className="flex items-center justify-center p-2">
+              <X />
+            </button>
+            <Link href={"/"}>Home</Link>
+            <Link href={"/companions"}>Companions</Link>
+            <Link href={"/my-journey"}>My journey</Link>
+          </div>
+          <div className="flex w-full flex-col justify-end items-center px-4">
+            {/* <Link href="/sign-in" className="w-full">
             <Button variant="outline" className="w-full mt-2">
               Sign Up
             </Button>
           </Link> */}
-          <SignedIn>
-            <UserButton showName={true} />
-          </SignedIn>
-           <SignedOut>
-          <SignInButton forceRedirectUrl={"/redirecting"}>
-            <Button className='bg-black text-white hover:bg-gray-800 w-full'>Sign In</Button>
-          </SignInButton>
-        
-        </SignedOut>
+            <SignedIn>
+              <UserButton showName={true} />
+            </SignedIn>
+            <div onClick={() => setIsOpen(false)} className="w-full">
+              <SignedOut>
+                <SignInButton forceRedirectUrl={"/redirecting"}>
+                  <Button className="bg-black text-white hover:bg-gray-800 w-full">
+                    Sign In
+                  </Button>
+                </SignInButton>
+              </SignedOut>
+            </div>
+          </div>
         </div>
-      </div>
       )}
-
-     
     </nav>
   );
 };
